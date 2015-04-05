d1a53e7487e4ef00392a88642564f52e7898260a
Guillaume Luchet <guillaume@geelweb.org>
2015-04-05 17:05:49 +0200

    define MIDDLEWARE_CLASSES

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -5,6 +5,15 @@ INSTALLED_APPS = (
 
 SECRET_KEY = '$-muc7nzd^9g9z^f+^8@^inpbpixz=)d8k%g%qsgrw*3+)^1t_'
 
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
